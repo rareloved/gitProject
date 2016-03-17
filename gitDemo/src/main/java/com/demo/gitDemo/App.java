fb3f296fea63d5849635a75aaae2a28e3fe9b3ab
zhangshouzheng <zhang_shouzheng@163.com>
2016-03-17 15:31:14 +0800

    new gitDemo
    myeclipse 10 create new gitDemo

---
+++ gitDemo/src/main/java/com/demo/gitDemo/App.java
@@ -0,0 +1,13 @@
+package com.demo.gitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
